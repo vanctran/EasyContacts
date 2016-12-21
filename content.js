b21539e6da2cf2da5ff6650d6b8a2c28f2c82ef3
van-tran <vct@pdx.edu>
2016-12-20 21:19:39 -0800

    Added message handling

--- content.js
+++ content.js
@@ -2,8 +2,17 @@
  * Created by Van on 12/20/16.
  */
 
+var tabDOM = document.getElementsByTagName('html').innerHTML;
+    tabDOM = tabDOM.toString();
+
+chrome.runtime.sendMessage({tabDOM: tabDOM}, function(response) {
+    console.log(response);
+});
+
+/*
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     if(msg.text === 'report_back') {
         sendResponse(document.all[0].outerHTML);
     }
 });
+*/
--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@
  * Created by Van on 12/20/16.
  */
 
+/*
 document.addEventListener('DOMContentLoaded', function () {
     var bg = chrome.extension.getBackgroundPage();
     var emailArray = bg.emailArray;
@@ -11,4 +12,23 @@ document.addEventListener('DOMContentLoaded', function () {
         document.write(emailArray[i]);
     }
 
+});
+*/
+
+/*
+function getEmails(tabDOM) {
+
+    var searchIn = tabDOM.innerHTML;
+    var stringContext = searchIn.toString();
+    emailArray = stringContext.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+    return;
+
+    //return document.body.innerHTML.toString().match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+}
+*/
+
+chrome.runtime.onMessage.addListener(function(tabDOM, sender, sendResponse) {
+    var emailArray = tabDOM.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+    $("#main").text(emailArray);
+    sendResponse("SUCCESS!");
 });
